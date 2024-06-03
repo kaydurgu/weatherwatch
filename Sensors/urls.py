1456965e13283c9ce49d68b8ff13f4096d0ead6d
kaydurgu <mzhanbolot@gmail.com>
2024-06-03 07:28:49 +0600

    Divided Permission class into 2 parts Staff and Admin   Views for Sensor model added

--- Sensors/urls.py
+++ Sensors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SensorListView
+from .views import SensorListView, SensorDetailView,SensorUpdateView
 urlpatterns = [
-    path('', SensorListView.as_view(), name = "sensor-list"),
-   # path('<int:pk/>', SensorListView.as_view(), name = "sensor-list")
+    path('', SensorListView.as_view(), name = "sensor-list"), #staff group needed
+    path('<int:pk>/', SensorDetailView.as_view(), name = "sensor-detail"),  # Staff group needed
+    path('update/<int:pk>/', SensorUpdateView.as_view(), name = "sensor-update"),
 ]
--- Sensors/views.py
+++ Sensors/views.py
@@ -3,7 +3,12 @@ from rest_framework import generics, permissions
 from .models import Sensor
 from .serializers import SensorSerializer
 # Create your views here.
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsStaff(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user.groups.filter(name='Staff').exists()  or request.user.groups.filter(name='Admin').exists()
+
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return request.user.groups.filter(name='Admin').exists()
@@ -11,8 +16,13 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class SensorListView(generics.ListAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
-    permission_classes = [permissions.IsAuthenticated and IsAdminOrReadOnly]
-#class SensorDetailView(generics.RetrieveAPIView):
-#    queryset = Sensor.objects.all()
-#    serializer_class = SensorSerializer
-#    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated and IsStaff]
+class SensorDetailView(generics.RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+    permission_classes = [permissions.IsAuthenticated and IsStaff]
+
+class SensorUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class =  SensorSerializer
+    permission_classes = [permissions.IsAuthenticated and IsAdmin]
